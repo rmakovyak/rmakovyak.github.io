b042701c6a3381f8fc8360025a20816c5e72289a
Roman Makovyak <roman.m@smart-host.com>
2020-01-09 16:26:34 +0200

    Deploy

---
+++ gatsby-config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  siteMetadata: {
+    title: `faredirect`,
+    contactEmail: `info@fare.direct`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@rmakovyak`,
+    menuLinks: [
+      {
+        name: "Product",
+        link: "/product",
+      },
+      {
+        name: "Pricing",
+        link: "/pricing",
+      },
+      {
+        name: "Team",
+        link: "/team",
+      },
+    ],
+    footerLinks: [
+      {
+        name: "Privacy policy",
+        link: "/privacy-policy",
+      },
+      {
+        name: "Terms & conditions",
+        link: "/terms",
+      },
+      {
+        name: "Cookie policy",
+        link: "/cookie-policy",
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ styles-2cccd6a205cba8535d47.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{190:function(n,o,c){},191:function(n,o,c){},192:function(n,o,c){},193:function(n,o,c){},194:function(n,o,c){},206:function(n,o,c){}}]);
+//# sourceMappingURL=styles-2cccd6a205cba8535d47.js.map
